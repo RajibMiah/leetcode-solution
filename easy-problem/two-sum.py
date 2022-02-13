e8ce08a2e9c91e239505691855ffe1b01a07f68e
rajibmiah <ahmedrajib942@gmail.com>
2022-02-13 18:22:49 +0600

    update

---
+++ easy-problem/two-sum.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        seen = {}
+        for i , value in enumerate(nums):
+            remaining = target - nums[i]
+            
+            if remaining in seen:
+                return [i , seen[remaining]]
+            
+            seen[value] = i
