4195fba2f6e42c72d4d417ad79cc225c0d468df5
rajibmiah <ahmedrajib942@gmail.com>
2022-03-20 06:21:09 +0600

    update

--- medium/5. Longest Palindromic Substring.py
+++ medium/5. Longest Palindromic Substring.py
@@ -1,33 +1,40 @@
 
 def longPali(s):
     n = len(s)
-    r = 0
-    max_string = ''
-    dp = [ [ 0 for i in range(n)]
-                for j in range(n)]
-    
+    dp = [[0 for i in range(n)]
+          for j in range(n)]
+    max_length = 0
+    start = 0
     for i in range(n):
         dp[i][i] = 1
 
-    for col in range(0 , n):
-        for row in range(0 ,col):
-            if(row == col-1 and s[col] == s[row]):
+    for col in range(1, n):
+        for row in range(0, col):
+            if(row == col-1 and s[row] == s[col]):
                 dp[row][col] = 1
-                if(max(r , row)):
-                  r = row
-                  max_string = s[row :col + 1]  
-               
-            elif(s[col] == s[row] and dp[row + 1][col -1] == 1):
+                if col > max_length:
+                    start = row
+                    max_length = 1
+
+            elif(s[col] == s[row] and dp[row + 1][col - 1] == 1):
                 dp[row][col] = 1
-                if(max(r , row)):
-                  r = row
-                  max_string = s[row :col + 1]  
-                
-    return max_string
+                if col > max_length:
+                    start = row
+                    max_length = col
+
+    print(start, max_length)
+
+    return s[start: max_length + 1]
 
 
-if  __name__ == '__main__':
-    s = "cbbd"
-    print(longPali(s))
-
-
+if __name__ == '__main__':
+    # s = "aaaaaaaaa"
+    # print(longPali(s))
+    # "aaaaabaaaaa"
+    # "aaajaabbbaac"
+    # "cbbd"
+    # "ac"
+    # "ccd"
+    st = "ccd"
+    l = longPali(st)
+    print(l)
