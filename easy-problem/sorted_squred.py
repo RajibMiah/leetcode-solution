f88e954a9279e3821f215b82a31cae2d7db6e1b7
rajibmiah <ahmedrajib942@gmail.com>
2022-08-27 21:45:26 +0600

    reduced time and space complexity on sorted_squred

--- easy-problem/sorted_squred.py
+++ easy-problem/sorted_squred.py
@@ -1,4 +1,22 @@
 
+# with TIME COMPLEXITY O(n) AND SPACE COMPLEXITY O(n)
+def sortedSqured(arr):
+    startingIdx = 0
+    endingIdx = len(arr) - 1
+    result = [0 for i in range(len(arr))]
+    for i in range(len(arr))[::-1]:
+        startValue = arr[startingIdx] * arr[startingIdx]
+        endingValue = arr[endingIdx] * arr[endingIdx]
+        if startValue > endingValue:
+            result[i] = startValue
+            startingIdx += 1
+        else:
+            result[i] = endingValue
+            endingIdx -= 1
+    return result
+
+
+# Broth Froce with time complextiy O(nlogn)
 def sortedSqured(arr):
     arr = [i*i for i in arr]  # It toke n time and space complexity
     arr.sort()  # nlogn time and space complexity
