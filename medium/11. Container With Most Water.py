37459437183e3eaf2dd3ffd3b59e9790549440e0
rajibmiah <ahmedrajib942@gmail.com>
2022-09-03 02:42:54 +0600

    update time complexity on water container leetcode problem o(n^2) to O(N)

--- medium/11. Container With Most Water.py
+++ medium/11. Container With Most Water.py
@@ -1,6 +1,24 @@
 
-# Bruth Force approach /checking every possibility
+# time complexity O(N) and time complexity O(1)
 def maxArea(height):
+    areaLength = len(height)
+    i = 0
+    j = areaLength - 1
+    max_area = 0
+    while(i < j):
+        new_area = min(height[i], height[j]) * (j - i)
+        max_area = max(max_area, new_area)
+        if(height[i] < height[j]):
+            i += 1
+        else:
+            j -= 1
+    return max_area
+
+# Bruth Force approach /checking every possibility .
+# the time complexity of this algorithm is O(N^2) and space complexity O(1)
+
+
+def _maxArea(height):
     areaLength = len(height)
     i = 0
     max_area = 0
@@ -8,12 +26,11 @@ def maxArea(height):
         j = i + 1
         while(j < areaLength):
             new_max_area = min(height[i], height[j]) * (j - i)
-            if (new_max_area > max_area):
-                max_area = new_max_area
+            max_area = max(max_area, new_max_area)
             j += 1
         i += 1
     return max_area
 
 
 print(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))  # max area 49
-print(maxArea([1 , 1]))
+print(maxArea([1, 1]))
