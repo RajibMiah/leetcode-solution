72e0262af13ab3d186745ee3675ac0796d83b4da
rajibmiah <ahmedrajib942@gmail.com>
2022-09-09 02:52:17 +0600

    upsolve 572 subtree matching problem

--- easy-problem/572. Subtree of Another Tree.py
+++ easy-problem/572. Subtree of Another Tree.py
@@ -1,42 +1,27 @@
-def isSubtree(root, subRoot):
-    if root == None:
-        return False
-    elif isSameTree(root, subRoot):
-        return True
-    else:
-        return isSubtree(root.left, subRoot) or isSubtree(root.right,  subRoot)
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSameTree(self, r, t):
+        if r == None or t == None:
+            return r == None and t == None
+        elif r.val == t.val:
+            return self.isSameTree(r.left, t.left) and self.isSameTree(r.right, t.right)
+        else:
+            return False
 
-
-def isSameTree(root, subRoot):
-    if root == None or subRoot == None:
-        return root == None and subRoot == None
-    elif root.val == subRoot.val:
-        return isSameTree(root.left, subRoot.left) or isSameTree(root.right, subRoot.right)
-    else:
-        return False
-
-
-class TreeNode(object):
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-
-# root
-a = TreeNode(3)  # [3,4,5,1,2]
-b = TreeNode(4)
-c = TreeNode(5)
-d = TreeNode(1)
-e = TreeNode(2)
-# subRoot
-f = TreeNode(4)
-g = TreeNode(1)
-h = TreeNode(2)
-
-a.left = b
-a.right = c
-b.left = d
-b.right = e
-
-print(isSubtree())
+    def isSubtree(self, root, subRoot):
+        """
+        :type root: TreeNode
+        :type subRoot: TreeNode
+        :rtype: bool
+        """
+        if root == None:
+            return False
+        if self.isSameTree(root, subRoot):
+            return True
+        else:
+            return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
