6bd94116ad5aba7d5081dd97b7bca69368cb2df1
rajibmiah <ahmedrajib942@gmail.com>
2022-09-12 00:36:34 +0600

    optimaized 388 leetcode counting bits problem by dp

--- easy-problem/338. Counting Bits.py
+++ easy-problem/338. Counting Bits.py
@@ -1,4 +1,23 @@
 
+
+# optimized problem with O (N) using dynamic programming
+class Solution(object):
+    def countBits(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        dp = [0] * (n + 1)
+        offset = 1
+
+        for i in range(1, n + 1):
+            if offset * 2 == i:
+                offset = i
+            dp[i] = 1 + dp[i - offset]
+
+        return dp
+
+
 # Bruth force approacch
 def _countBits(n):
     """
