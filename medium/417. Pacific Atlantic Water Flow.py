3366f776e4ab32bd923b7e11081362c6035a539e
rajibmiah <ahmedrajib942@gmail.com>
2022-09-18 14:00:00 +0600

    leetcode 417 pacificatlantic problme solved

---
+++ medium/417. Pacific Atlantic Water Flow.py
@@ -0,0 +1,31 @@
+def pacificAtlantic(heights):
+    rows, cols = len(heights), len(heights[0])
+    pacific, atlantic = set(), set()
+
+    def dfs(r, c, visit, preHeight):
+        if ((r, c) in visit or r < 0 or c < 0 or r == rows or c == cols or heights[r][c] < preHeight):
+            return
+        visit.add((r, c))
+        dfs(r + 1, c, visit, heights[r][c])
+        dfs(r - 1, c, visit, heights[r][c])
+        dfs(r, c + 1, visit, heights[r][c])
+        dfs(r, c - 1, visit, heights[r][c])
+
+    for c in range(cols):
+        dfs(0, c, pacific, heights[0][c])
+        dfs(rows-1, c, atlantic, heights[rows - 1][c])
+
+    for r in range(rows):
+        dfs(r, 0, pacific, heights[r][0])
+        dfs(r, cols - 1, atlantic, heights[r][cols - 1])
+    result = []
+    for r in range(rows):
+        for c in range(cols):
+            if (r, c) in pacific and (r, c) in atlantic:
+                result.append([r, c])
+
+    return result
+
+
+print(pacificAtlantic([[1, 2, 2, 3, 5], [3, 2, 3, 4, 4], [
+      2, 4, 5, 3, 1], [6, 7, 1, 4, 5], [5, 1, 1, 2, 4]]))
