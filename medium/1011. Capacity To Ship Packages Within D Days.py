f993ac8d66df4718859e30c1c5d9f9465ea41534
rajibmiah <ahmedrajib942@gmail.com>
2022-10-12 06:25:39 +0600

    solved medium problem 1011 capacity to shipment

---
+++ medium/1011. Capacity To Ship Packages Within D Days.py
@@ -0,0 +1,31 @@
+def shipWithinDays(weights, days):
+    def canShip(opacity):
+        day = 1
+        remain = opacity
+        for weight in weights:
+            if weight > opacity:
+                return False
+            remain -= weight
+            if remain < 0:
+                day += 1
+                remain = opacity - weight
+        return day <= days
+
+    low = max(weights)
+    high = 0
+
+    for weight in weights:
+        high += weight
+
+    while(low < high):
+
+        mid = (low + high) // 2
+
+        if canShip(mid):
+            high = mid
+        else:
+            low = mid + 1
+    return low
+
+
+print(shipWithinDays([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5))  # Output: 15
