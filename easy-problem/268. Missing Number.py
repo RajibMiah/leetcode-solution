fffc1d505ba69d6608ae89ef9605b27df016399d
rajibmiah <ahmedrajib942@gmail.com>
2022-11-02 03:37:46 +0600

    update

--- easy-problem/268. Missing Number.py
+++ easy-problem/268. Missing Number.py
@@ -1,17 +1,37 @@
 def missingNumber(nums):
-  
+
     def binarySearch(nums):
-        l , r = 0 , len(nums)-1
+        l, r = 0, len(nums)-1
         res = len(nums)
         while(l <= r):
-            mid = l + ( r - l) //2
+            mid = l + (r - l) // 2
             if nums[mid] > mid:
-                res , r = mid , mid - 1
+                res, r = mid, mid - 1
             else:
                 l = mid + 1
-                
+
         return res
-    
+
     return binarySearch(sorted(nums))
 
-print(missingNumber([3,0,1])) #output 2
+
+print(missingNumber([3, 0, 1]))  # output 2
+
+
+def missingNumber(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    l, r = 0, len(nums) - 1
+
+    res = len(nums)
+    nums.sort()
+    while(l <= r):
+        mid = l + (r - l) // 2
+
+        if nums[mid] > mid:
+            res, r = mid, mid - 1
+        else:
+            l = mid + 1
+    return res
